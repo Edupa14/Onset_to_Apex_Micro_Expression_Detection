33b2edba49e0eeea6520352164b8f60f189f506d
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-08-18 10:27:54 +0800

    Create CASMEII_Dataset_Validator.py

---
+++ Fuse/CASMEII/CASMEII_Dataset_Validator.py
@@ -0,0 +1,31 @@
+import numpy
+import os
+
+segmentName = 'UpperFace_SelectiveDivideAndConquer'
+sizeH = 128
+sizeV = 128
+sizeD = 2
+
+
+segment_traininglabels = numpy.load('numpy_training_datasets/{0}_labels_{1}x{2}x{3}.npy'.format(segmentName,sizeH, sizeV,sizeD))
+print(segment_traininglabels)
+cat=[0]*5
+for item in segment_traininglabels:
+    for c in range(len(cat)):
+        if item[c]==1:
+            cat[c]+=1
+
+print(cat)
+
+
+
+
+cat=[0]*7
+dir=0
+for typepath in os.listdir('../../CASMEII_categorical_apex_SelectiveDivideAndConquer/'):
+    directorylisting = os.listdir('../../CASMEII_categorical_apex_SelectiveDivideAndConquer/'+typepath)
+    for video in directorylisting:
+        cat[dir]+=1
+    dir+=1
+print(cat)
+print(sum(cat))
