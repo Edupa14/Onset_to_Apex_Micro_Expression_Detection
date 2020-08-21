edc3e89aa25ff8ea41a9dbfc58e01a7fab41fc28
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-08-21 23:41:29 +0800

    Update SIMC_UFFMEDN.py

--- SIMC/SIMC_UFFMEDN.py
+++ SIMC/SIMC_UFFMEDN.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score,f1_score
 from keras.models import Sequential, Model
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution3D, MaxPooling3D, ZeroPadding3D
-from keras.layers import LeakyReLU ,PReLU
+from keras.layers import LeakyReLU ,PReLU,BatchNormalization,concatenate,Input
 from keras.callbacks import ModelCheckpoint,EarlyStopping,ReduceLROnPlateau,Callback
 from sklearn.model_selection import train_test_split,LeaveOneOut,KFold
 from keras import backend as K
@@ -18,42 +18,80 @@ class myCallback(Callback):
             self.model.stop_training = True
 
 def evaluate(segment_train_images, segment_validation_images, segment_train_labels, segment_validation_labels,test_index ):
-
-    model = Sequential()
-    #model.add(ZeroPadding3D((2,2,0)))
-    model.add(
-        Convolution3D(32, (20, 20, 1), strides=(10, 10, 1), input_shape=(1, sizeH, sizeV, sizeD), padding='Same'))
-    model.add(PReLU())
-
-    # model.add(Dropout(0.5))
-    #3
-    # model.add(Convolution3D(32, (3, 3, 2), strides=1, padding='Same'))
-    # model.add(PReLU())
-    #4
-    # model.add(Dropout(0.5))
-    #1
-    model.add(MaxPooling3D(pool_size=(3, 3, 2)))
-    model.add( PReLU())
-    #2
-    # model.add(Dropout(0.5))
-    model.add(Flatten())
-    # model.add(Dense(1024, init='normal'))
-    # model.add(Dropout(0.5))
-    # model.add(Dense(128, init='normal'))
-    # model.add(Dropout(0.5))
-    model.add(Dense(3, init='normal'))
-    model.add(Dropout(0.5))
-    model.add(Activation('softmax'))
+    layer_in = Input(shape=(1, sizeH, sizeV, sizeD))
+    # conv1 = Convolution3D(256, (20, 20, 9), strides=(10, 10, 3), padding='Same')(input)
+    # # bn1=BatchNormalization()(conv1)
+    # ract_1 = PReLU()(conv1)
+    conv1 = Convolution3D(96, (20, 20, 1), strides=(10, 10, 1), padding='same', activation='relu')(layer_in)
+    # 3x3 conv
+    conv3 = Convolution3D(256, (20, 20, 1), strides=(10, 10, 1), padding='same', activation='relu')(layer_in)
+    conv3 = Convolution3D(512, (3, 3, 1), padding='same', activation='relu')(conv3)
+    # 5x5 conv
+    # conv5 = Convolution3D(16, (20, 20, 1), strides=(10, 10, 1), padding='same', activation='relu')(layer_in)
+    # conv5 = Convolution3D(32, (5, 5, 1), padding='same', activation='relu')(conv5)
+    # 3x3 max pooling
+    pool = MaxPooling3D((3, 3, 3), strides=(1, 1, 1), padding='same')(layer_in)
+    pool = Convolution3D(32, (20, 20, 1), strides=(10, 10, 1), padding='same', activation='relu')(pool)
+    # concatenate filters, assumes filters/channels last
+    layer_out = concatenate([conv1, conv3,  pool], axis=-4)
+    # add1= Add() ([conv3,ract_1])
+    # drop0 = Dropout(0.5)(layer_out)
+    # conv6 = Convolution3D(512, (3, 3, 3), strides=1, padding='Same')(drop0)
+    # # bn3 = BatchNormalization()(conv3)
+    ract_4 = PReLU()(layer_out)
+    flatten_1 = Flatten()(ract_4)
+    # dense_1 = Dense(1024, init='normal')(flatten_1)
+    # dense_2 = Dense(128, init='normal')(dense_1)
+    dense_3 = Dense(3, init='normal')(flatten_1)
+    # drop1 = Dropout(0.5)(dense_3)
+    activation = Activation('softmax')(dense_3)
     opt = SGD(lr=0.01)
+    model = Model(inputs=[layer_in], outputs=activation)
     model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+# ----------------------------
+#     model = Sequential()
+#     # model.add(ZeroPadding3D((2,2,0)))
+#     model.add(Convolution3D(32, (6, 6, 1), strides=(3, 3, 1), input_shape=(1, sizeH, sizeV, sizeD), padding='Same'))
+#
+#     model.add(Convolution3D(64, (12, 12, 1), strides=(6, 6, 1), input_shape=(1, sizeH, sizeV, sizeD), padding='Same'))
+#     model.add(PReLU())
+#     # model.add(Convolution3D(128, (8, 8, 1), strides=1, input_shape=(1, sizeH, sizeV, sizeD), padding='Same'))
+#     # model.add(PReLU())
+#     # model.add(Dropout(0.5))
+#     # 3
+#     # model.add(Convolution3D(32, (3, 3, 2), strides=1, padding='Same'))
+#     # model.add(PReLU())
+#     # 40
+#     # model.add(Dropout(0.5))
+#     # 1
+#     model.add(MaxPooling3D(pool_size=(3, 3, 2)))
+#     model.add(PReLU())
+#     # 2
+#     # model.add(Dropout(0.5))
+#     model.add(Flatten())
+#     model.add(Dense(256, init='normal'))
+#     # model.add(Dropout(0.5))
+#     model.add(Dense(128, init='normal'))
+#     # model.add(PReLU())
+#     # model.add(Dense(128, init='normal'))`
+#     model.add(Dropout(0.5))
+#     model.add(Dense(5, init='normal'))
+#     # model.add(Dropout(0.5))
+#     model.add(Activation('softmax'))
+#     opt = SGD(lr=0.1)
+#     model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
     model.summary()
 
-    filepath="weights_SIMC/weights-improvement"+str(test_index)+"-{epoch:02d}-{val_acc:.2f}.hdf5"
+    filepath = "weights_CAS(ME)2/weights-improvement" + str(test_index) + "-{epoch:02d}-{val_acc:.2f}.hdf5"
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    EarlyStop = EarlyStopping(monitor='val_acc', min_delta=0, patience=50, restore_best_weights=True, verbose=1, mode='max')
-    reduce = ReduceLROnPlateau(monitor='val_acc', factor=0.5, patience=30,cooldown=10, verbose=1,min_delta=0, mode='max',min_lr=0.0005)
-    callbacks_list = [ EarlyStop, reduce,myCallback()]
+    EarlyStop = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, restore_best_weights=True, verbose=1,
+                              mode='max')
+    reduce = ReduceLROnPlateau(monitor='val_acc', factor=0.2, patience=5, cooldown=1, verbose=1, min_delta=0,
+                               mode='max', min_lr=0.0005)
+    callbacks_list = [EarlyStop, reduce, myCallback()]
+
+
 
 
 
@@ -62,7 +100,7 @@ def evaluate(segment_train_images, segment_validation_images, segment_train_labe
 
     # Training the model
 
-    history = model.fit(segment_train_images, segment_train_labels, validation_data = (segment_validation_images, segment_validation_labels), callbacks=callbacks_list, batch_size = 16, nb_epoch = 500, shuffle=True,verbose=1)
+    history = model.fit(segment_train_images, segment_train_labels, validation_data = (segment_validation_images, segment_validation_labels), callbacks=callbacks_list, batch_size = 8, nb_epoch = 500, shuffle=True,verbose=1)
 
 
 
@@ -213,7 +251,7 @@ segmentName = 'UpperFace'
 sizeH = 32
 sizeV = 32
 sizeD = 2
-testtype = "kfold"
+testtype = "loocv"
 ####################################
 
 # Load training images and labels that are stored in numpy array
@@ -222,6 +260,12 @@ segment_training_set = numpy.load(
     'numpy_training_datasets/{0}_images_{1}x{2}x{3}.npy'.format(segmentName, sizeH, sizeV, sizeD))
 segment_traininglabels = numpy.load(
     'numpy_training_datasets/{0}_labels_{1}x{2}x{3}.npy'.format(segmentName, sizeH, sizeV, sizeD))
+
+
+# print(segment_traininglabels)
+# print(numpy.sum(segment_traininglabels,axis=0))
+
+
 
 if testtype == "kfold":
     val_labels, pred_labels = kfold()
