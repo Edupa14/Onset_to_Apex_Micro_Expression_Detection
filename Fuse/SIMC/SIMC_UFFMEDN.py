6497344134ae6528752a3722a98271297ebd0cad
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-08-23 15:45:30 +0800

    Update SIMC_UFFMEDN.py

--- Fuse/SIMC/SIMC_UFFMEDN.py
+++ Fuse/SIMC/SIMC_UFFMEDN.py
@@ -43,9 +43,9 @@ def evaluate(segment_train_images, segment_validation_images, segment_train_labe
     # dense_1 = Dense(1024, init='normal')(flatten_1)
     # dense_2 = Dense(128, init='normal')(dense_1)
     layer_in2 = Input(shape=(1, sizeH2, sizeV2, sizeD2))
-    conv21 = Convolution3D(96, (20, 20, 30), strides=(10, 10, 15), padding='Same')(layer_in2)
+    conv21 = Convolution3D(32, (20, 20, 30), strides=(10, 10, 15), padding='Same')(layer_in2)
     ract_21 = PReLU()(conv21)
-    conv22 = Convolution3D(256, (3, 3, 3), strides=1, padding='Same')(ract_21)
+    conv22 = Convolution3D(32, (3, 3, 3), strides=1, padding='Same')(ract_21)
     ract_22 = PReLU()(conv22)
     flatten_2 = Flatten()(ract_22)
 
@@ -265,17 +265,17 @@ def kfold():
 # edit params
 K.set_image_dim_ordering('th')
 
-segmentName = 'UpperFace'
+segmentName = 'FullFace'
 sizeH = 32
 sizeV = 32
 sizeD = 2
-segmentName2 = 'UpperFace_cat'
+segmentName2 = 'FullFace_cat'
 sizeH2 = 32
 sizeV2 = 32
 sizeD2 = 30
 testtype = "kfold"
 ###################################
-notes="96*256"
+notes="9*3"
 ####################################
 
 # Load training images and labels that are stored in numpy array
