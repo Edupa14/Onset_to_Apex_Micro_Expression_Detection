01fd8df3405e1b3627bb7b1aa154d5bf076aad03
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-09-01 02:44:54 +0800

    Update SAMM_Apex_evaluator.py

--- SAMM/SAMM_Apex_evaluator.py
+++ SAMM/SAMM_Apex_evaluator.py
@@ -3,26 +3,35 @@ import numpy
 import os
 import math
 import statistics as stat
-catdatafile = pd.read_excel('../../CAS(ME)^2code_final(Updated).xlsx')
+catdatafile = pd.read_excel('../../SAMM_Micro_FACS_Codes_v2.xlsx')
 data = numpy.array(catdatafile)
-namedatafile = pd.read_excel('../../name.xlsx')
-namedata = numpy.array(namedatafile)
-count=0
-for item in data:
-    for item2 in namedata:
-        # print(str(item2[0]),"s"+str(item[0]))
-        if str(item2[1])==str(item[0]):
-            # print(item2[0])
-            item[0]=item2[0]
-            break
-path='../../CAS(ME)2_categorical_apex_SelectiveDivideAndConquer/'
-negativepath = path+'negative/'
-positivepath = path+'positive/'
-surprisepath = path+'surprise/'
-othersepath = path+'others/'
+# namedatafile = pd.read_excel('../../name.xlsx')
+# namedata = numpy.array(namedatafile)
+# count=0
+# for item in data:
+#     for item2 in namedata:
+#         # print(str(item2[0]),"s"+str(item[0]))
+#         if str(item2[1])==str(item[0]):
+#             # print(item2[0])
+#             item[0]=item2[0]
+#             break
+path='../../SAMM_categorical_apex_SelectiveDivideAndConquer/'
+angerpath = path+'Anger/'
+sadnesspath =path+'Sadness/'
+happinesspath =path+'Happiness/'
+disgustpath =path+'Disgust/'
+fearpath = path+'Fear/'
+surprisepath = path+'Surprise/'
+contemptpath = path+'Contempt/'
+otherpath = path+'Other/'
+segmentName = 'UpperFace_DivideAndConquer'
+sizeH=32
+sizeV=32
+sizeD=2
+
+paths=[angerpath,sadnesspath,disgustpath, fearpath, happinesspath,surprisepath,contemptpath,otherpath]
 
 
-paths=[negativepath,positivepath, surprisepath,othersepath]
 diffs=[]
 count=0
 for pi in range(len(paths)):
@@ -33,7 +42,7 @@ for pi in range(len(paths)):
             print(directorylisting[video])
             for item in data:
                 # print(str(item[0])+"_"+str(item[1]),directorylisting[video])
-                if str(item[0])+"_"+str(item[1])==directorylisting[video]:
+                if str(item[1])==directorylisting[video]:
                     # Framefound=False
                     # first=None
                     # last=None
@@ -49,8 +58,9 @@ for pi in range(len(paths)):
                     directorylistingvid = os.listdir(paths[pi]+directorylisting[video])
                     for pic in directorylistingvid:
                         if pic[0]=='2':
-                            print(pic[4:-4])
-                            diffs.append(abs(item[4]-int(pic[4:-4])))
+                            print(pic[5:-4])
+                            diffs.append(abs(item[4]-int(pic[5:-4])))
+                            print("diff",abs(item[4]-int(pic[5:-4])))
                     # print(directorylistingvid)
                     # print(first)
                     # print(framelistinglist[count][first-1:last+2],framelistinglist[count][first],framelistinglist[count][last])
