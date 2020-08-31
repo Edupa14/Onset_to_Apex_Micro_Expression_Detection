5b093ba1ce686a37a9eec167f29b5b857bc80179
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-09-01 01:46:04 +0800

    Create CAS(ME)2_Apex_evaluator.py

---
+++ CAS(ME)2/CAS(ME)2_Apex_evaluator.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy
+import os
+import math
+import statistics as stat
+disgustpath = '../../CASMEII_categorical_apex_DivideAndConquer/disgust/'
+fearpath = '../../CASMEII_categorical_apex_DivideAndConquer/fear/'
+happinesspath = '../../CASMEII_categorical_apex_DivideAndConquer/happiness/'
+otherspath = '../../CASMEII_categorical_apex_DivideAndConquer/others/'
+repressionpath = '../../CASMEII_categorical_apex_DivideAndConquer/repression/'
+sadnesspath = '../../CASMEII_categorical_apex_DivideAndConquer/sadness/'
+surprisepath = '../../CASMEII_categorical_apex_DivideAndConquer/surprise/'
+
+
+
+paths=[disgustpath,  happinesspath,otherspath,repressionpath,surprisepath]
+catdatafile = pd.read_excel('../../cat_apex.xlsx')
+data = numpy.array(catdatafile)
+diffs=[]
+count=0
+for pi in range(len(paths)):
+    directorylisting = os.listdir(paths[pi])
+    print(pi)
+    for video in range(len(directorylisting)):
+        if directorylisting[video]!="4_EP12_01f":
+            print(directorylisting[video])
+            for item in data:
+                # print(str(item[0])+"_"+str(item[1]),directorylisting[video])
+                if str(item[0])+"_"+str(item[1])==directorylisting[video]:
+                    # Framefound=False
+                    # first=None
+                    # last=None
+                    # print(framelistinglist[count])
+                    # for frame in range(len(framelistinglist[count])):
+                    #     if framelistinglist[count][frame]==item[4]-item[3]:
+                    #         Framefound=True
+                    #         first=frame-1
+                    #     # elif Framefound==True:
+                    #     #     last=frame-1
+                    #         break
+                    print(int(item[4]))
+                    directorylistingvid = os.listdir(paths[pi]+directorylisting[video])
+                    for pic in directorylistingvid:
+                        if pic[0]=='2':
+                            print(pic[4:-4])
+                            diffs.append(abs(item[4]-int(pic[4:-4])))
+                    # print(directorylistingvid)
+                    # print(first)
+                    # print(framelistinglist[count][first-1:last+2],framelistinglist[count][first],framelistinglist[count][last])
+                    # print(item[4]-item[3])
+                    count+=1
+                    break
+print("MAE: ",(sum(diffs))/len(diffs))
+print("SD: ",stat.stdev(diffs))
+print("SE: ",stat.stdev(diffs)/(math.sqrt(len(diffs))))
