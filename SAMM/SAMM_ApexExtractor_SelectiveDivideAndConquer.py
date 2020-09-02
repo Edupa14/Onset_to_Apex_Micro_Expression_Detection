60c2efd5601b8b3a926a82aa6bbc12a06adc05f2
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-09-03 00:16:54 +0800

    Update SAMM_ApexExtractor_SelectiveDivideAndConquer.py

--- SAMM/SAMM_ApexExtractor_SelectiveDivideAndConquer.py
+++ SAMM/SAMM_ApexExtractor_SelectiveDivideAndConquer.py
@@ -4,7 +4,7 @@ import cv2
 import dlib
 from keras import backend as K
 import shutil
-
+import math
 K.set_image_dim_ordering('th')
 
 # DLib Face Detection path setup
@@ -39,6 +39,61 @@ def annotate_landmarks(img, landmarks, font_scale=0.4):
         cv2.circle(img, pos, 3, color=(0, 255, 255))
     return img
 
+
+def generate_pos(imgs,landmark_list):
+    image = cv2.imread(videopath + '/' + imgs[0])
+    landmarks = get_landmark(image)
+    numpylandmarks = np.asarray(landmarks)
+    landmarksNos = len(numpylandmarks)
+    total_pos = [1] * landmarksNos
+    img_pos = []
+    count = 0
+
+    for image in viddirectorylisting:
+        img_pos.append([])
+        image = cv2.imread(videopath + '/' + image)
+        landmarks = get_landmark(image)
+        numpylandmarks = np.asarray(landmarks)
+        count += 1
+        img_pos[count - 1] = [1] * landmarksNos
+        for pos in range(landmarksNos):
+            # total_pos[pos] += (numpylandmarks[pos] - numpylandmarks[8])
+            # print(numpylandmarks[pos], numpylandmarks[8], numpylandmarks[pos] - numpylandmarks[8])
+            img_pos[count - 1][pos] = (numpylandmarks[pos] - numpylandmarks[8])
+    return img_pos
+def new_find_max(start,end,img_pos,vidlist,landmark_list):
+    total_pos=[np.zeros(shape=2,dtype=int) for _ in range(len(img_pos[0]))]
+    for imgval in img_pos:
+        for posi in range(len(imgval)):
+            # print(temp_tot[posi])
+            total_pos[posi]+=imgval[posi]
+            # print('a',temp_tot[posi],temp_tot)
+    avg_pos = []
+    for pos in total_pos:
+        avg_pos.append(pos / (end-start+1))
+    # print(avg_pos[0],img_pos[0][0],avg_pos[0]-img_pos[0][0])
+    max_diff = 0
+    max_diff_image = None
+    for image in range(start,end+1,1):
+        diff = []
+        # print(landmark_list,len(avg_pos))
+        for pos in landmark_list:
+            diff.append(abs(avg_pos[pos] - img_pos[image][pos]))
+        # print(sum(diff),len(diff),sum(sum(diff)/len(diff)))
+        avg_diff = abs(sum(sum(diff) / len(diff)))
+        # print(max_diff,avg_diff)
+        if math.isnan(avg_diff) :
+            print(avg_pos)
+            print(img_pos[image])
+            print(sum(diff) , len(diff))
+        if max_diff < avg_diff:
+            max_diff = avg_diff
+            max_diff_image = image
+    print(start,end,len(viddirectorylisting))
+    if max_diff_image==None:
+        print(avg_diff)
+        print(avg_pos)
+    return max_diff,vidlist[max_diff_image]
 
 
 def find_max(imgs):
@@ -94,7 +149,7 @@ def find_max(imgs):
 path='../../SAMM_categorical/'
 
 
-targetpath= '../../SAMM_categorical_apex_SelectiveDivideAndConquer_NEW/'
+targetpath= '../../SAMM_categorical_apex_SelectiveDivideAndConquer_NEW_mod/'
 if os.path.exists(targetpath ):
     shutil.rmtree(targetpath )
 os.mkdir(targetpath , mode=0o777)
@@ -110,6 +165,10 @@ for item in directorylisting:
             shutil.rmtree(targetpath+item)
         os.mkdir(targetpath+item, mode=0o777)
 counter01=1
+
+landmark_list = [x for x in range(17, 27)]
+landmark_list.extend([x for x in range(36, 68)])
+
 for subject in directorylisting:
     # print(subject)
     subjectdirectorylisting=os.listdir(path+subject)
@@ -117,6 +176,7 @@ for subject in directorylisting:
         videopath = path+subject +'/'+ video
         newvideopath=targetpath+subject +'/'+ video
         found=False
+
         # for vidid in range(len(catdata)):
             # if catdata[vidid][1]==str(video) and ("sub"+str(catdata[vidid][0])==subject or "sub0"+str(catdata[vidid][0])==subject):
             #     print(video,catdata[vidid][2],catdata[vidid])
@@ -136,14 +196,17 @@ for subject in directorylisting:
         Rval=None
         lenght=len(viddirectorylisting)
         imgs=viddirectorylisting
-        while lenght>2:
-            Lmax,Lval=find_max(imgs[:int(len(imgs)//2)])
-            Rmax,Rval = find_max(imgs[int(len(imgs)//2):])
+        img_pos=generate_pos(imgs,landmark_list)
+        start=0
+        end=len(imgs)-1
+        while lenght>=2:
+            Lmax,Lval=new_find_max(start,start+int((end-start)//2),img_pos,imgs,landmark_list)
+            Rmax,Rval = new_find_max(start+int((end-start)//2)+1,end,img_pos,imgs,landmark_list)
             if Lmax>Rmax:
-                imgs=imgs[:int(len(imgs)//2)]
+                end=start+int((end-start)//2)
             else:
-                imgs=imgs[int(len(imgs)//2):]
-            lenght=len(imgs)
+                start=start+int((end-start)//2)
+            lenght=end-start
         if Lmax > Rmax:
             max_diff_image=Lval
             max_diff=Lmax
