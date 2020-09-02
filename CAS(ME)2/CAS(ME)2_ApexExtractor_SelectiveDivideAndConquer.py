8e1e7bff6bc46a748b306e6c99ca5806529c0179
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-09-02 22:51:09 +0800

    Update CAS(ME)2_ApexExtractor_SelectiveDivideAndConquer.py

--- CAS(ME)2/CAS(ME)2_ApexExtractor_SelectiveDivideAndConquer.py
+++ CAS(ME)2/CAS(ME)2_ApexExtractor_SelectiveDivideAndConquer.py
@@ -79,19 +79,68 @@ def find_max(imgs):
             max_diff_image = image
     return max_diff,max_diff_image
 
+def generate_pos(imgs,landmark_list):
+    image = cv2.imread(videopath + '/' + imgs[0])
+    landmarks = get_landmark(image)
+    numpylandmarks = np.asarray(landmarks)
+    landmarksNos = len(numpylandmarks)
+    total_pos = [1] * landmarksNos
+    img_pos = []
+    count = 0
+
+    for image in viddirectorylisting:
+        img_pos.append([])
+        image = cv2.imread(videopath + '/' + image)
+        landmarks = get_landmark(image)
+        numpylandmarks = np.asarray(landmarks)
+        count += 1
+        img_pos[count - 1] = [1] * landmarksNos
+        for pos in range(landmarksNos):
+            # total_pos[pos] += (numpylandmarks[pos] - numpylandmarks[8])
+            # print(numpylandmarks[pos], numpylandmarks[8], numpylandmarks[pos] - numpylandmarks[8])
+            img_pos[count - 1][pos] = (numpylandmarks[pos] - numpylandmarks[8])
+    return img_pos
+def new_find_max(start,end,img_pos,vidlist,landmark_list):
+    total_pos=[np.zeros(shape=2,dtype=int) for _ in range(len(img_pos[0]))]
+    for imgval in img_pos:
+        for posi in range(len(imgval)):
+            # print(temp_tot[posi])
+            total_pos[posi]+=imgval[posi]
+            # print('a',temp_tot[posi],temp_tot)
+    avg_pos = []
+    for pos in total_pos:
+        avg_pos.append(pos / count)
+    # print(avg_pos[0],img_pos[0][0],avg_pos[0]-img_pos[0][0])
+    max_diff = 0
+    max_diff_image = None
+    count2 = 0
+    for image in range(start,end+1,1):
+        diff = []
+        # print(landmark_list,len(avg_pos))
+        for pos in landmark_list:
+            diff.append(abs(avg_pos[pos] - img_pos[count2][pos]))
+        count2 += 1
+        # print(sum(diff),len(diff),sum(sum(diff)/len(diff)))
+        avg_diff = abs(sum(sum(diff) / len(diff)))
+        # print(max_diff,avg_diff)
+        if max_diff < avg_diff:
+            max_diff = avg_diff
+            max_diff_image = image
+    print(start,end,len(viddirectorylisting))
+    return max_diff,vidlist[max_diff_image]
+
 
 path='../../CAS(ME)2_categorical/'
 
 
 
 
-targetpath= '../../CAS(ME)2_categorical_apex_SelectiveDivideAndConquer_NEW/'
+targetpath= '../../CAS(ME)2_categorical_apex_SelectiveDivideAndConquer_NEW_mod/'
 if os.path.exists(targetpath ):
     shutil.rmtree(targetpath )
 os.mkdir(targetpath , mode=0o777)
 directorylisting = os.listdir(path)
 
-counter01=1
 count=0
 cats=[]
 for item in directorylisting:
@@ -100,6 +149,10 @@ for item in directorylisting:
         if os.path.exists(targetpath+item):
             shutil.rmtree(targetpath+item)
         os.mkdir(targetpath+item, mode=0o777)
+counter01=1
+
+landmark_list = [x for x in range(17, 27)]
+landmark_list.extend([x for x in range(36, 68)])
 
 for subject in directorylisting:
     # print(subject)
@@ -108,6 +161,7 @@ for subject in directorylisting:
         videopath = path+subject +'/'+ video
         newvideopath=targetpath+subject +'/'+ video
         found=False
+
         # for vidid in range(len(catdata)):
             # if catdata[vidid][1]==str(video) and ("sub"+str(catdata[vidid][0])==subject or "sub0"+str(catdata[vidid][0])==subject):
             #     print(video,catdata[vidid][2],catdata[vidid])
@@ -115,8 +169,8 @@ for subject in directorylisting:
         if os.path.exists(newvideopath):
             shutil.rmtree(newvideopath)
         os.mkdir(newvideopath, mode=0o777)
-        print(counter01)
         viddirectorylisting = os.listdir(videopath)
+        print(counter01)
         print(videopath,viddirectorylisting)
         viddirectorylisting.sort()
         print(videopath, viddirectorylisting)
@@ -127,30 +181,33 @@ for subject in directorylisting:
         Rval=None
         lenght=len(viddirectorylisting)
         imgs=viddirectorylisting
-        while lenght>2:
-            Lmax,Lval=find_max(imgs[:int(len(imgs)//2)])
-            Rmax,Rval = find_max(imgs[int(len(imgs)//2):])
+        img_pos=generate_pos(imgs,landmark_list)
+        start=0
+        end=len(imgs)-1
+        while lenght>=2:
+            Lmax,Lval=new_find_max(start,start+int((end-start)//2),img_pos,imgs,landmark_list)
+            Rmax,Rval = new_find_max(start+int((end-start)//2)+1,end,img_pos,imgs,landmark_list)
             if Lmax>Rmax:
-                imgs=imgs[:int(len(imgs)//2)]
+                end=start+int((end-start)//2)
             else:
-                imgs=imgs[int(len(imgs)//2):]
-            lenght=len(imgs)
+                start=start+int((end-start)//2)
+            lenght=end-start
         if Lmax > Rmax:
             max_diff_image=Lval
             max_diff=Lmax
         else:
             max_diff_image=Rval
-            max_diff=Rmax
-        print(max_diff_image,max_diff)
+            # max_diff=Rmax
+        # print(max_diff_image,max_diff)
         for image in viddirectorylisting:
             if viddirectorylisting[0] == image :
                 # print(vidid, image,1)
-                print(videopath + "/" + str(image))
+                # print(videopath + "/" + str(image))
                 shutil.copyfile(videopath + "/" + str(image),
                                 newvideopath+ '/1' + str(image))
             if  max_diff_image == image:
                 # print(vidid,image,2)
-                print(videopath+"/"+str(image))
+                # print(videopath+"/"+str(image))
                 shutil.copyfile(videopath+"/"+str(image), newvideopath+'/2'+str(image))
         counter01+=1
 
