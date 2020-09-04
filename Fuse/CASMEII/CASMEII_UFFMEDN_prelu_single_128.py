f9d6fdbfa0c8280025a15ba0906f808da0c4f478
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-09-04 12:49:36 +0800

    Update CASMEII_UFFMEDN_prelu_single_128.py

--- Fuse/CASMEII/CASMEII_UFFMEDN_prelu_single_128.py
+++ Fuse/CASMEII/CASMEII_UFFMEDN_prelu_single_128.py
@@ -47,8 +47,22 @@ def evaluate(segment_train_images, segment_validation_images, segment_train_labe
     # drop0 = Dropout(0.5)(layer_out)
     # conv6 = Convolution3D(512, (3, 3, 3), strides=1, padding='Same')(drop0)
     # # bn3 = BatchNormalization()(conv3)
+
     ract_4 = PReLU()(layer_out)
     flatten_1 = Flatten()(ract_4)
+
+    # conv15 = Convolution2D(96, (5, 5), strides=1, padding='same')(layer_out)
+    # ract_2115 = PReLU()(conv15)
+    # conv14 = Convolution2D(96, (3, 3), strides=1, padding='same')(ract_2115)
+    # ract_2114 = PReLU()(conv14)
+    #
+    # # add1= Add() ([conv3,ract_1])
+    # # drop0 = Dropout(0.5)(layer_out)
+    # # conv6 = Convolution3D(512, (3, 3, 3), strides=1, padding='Same')(drop0)
+    # # # bn3 = BatchNormalization()(conv3)
+    # # ract_4 = PReLU()(layer_out)
+    # flatten_1 = Flatten()(ract_2114)
+
     # dense_1 = Dense(1024, init='normal')(flatten_1)
     # dense_2 = Dense(128, init='normal')(dense_1)
     layer_in2 = Input(shape=(1, sizeH2, sizeV2, sizeD2))
@@ -275,8 +289,8 @@ def kfold():
 K.set_image_dim_ordering('th')
 
 segmentName = 'UpperFace_SelectiveDivideAndConquer_NEW_mod'
-sizeH = 32
-sizeV = 32
+sizeH = 128
+sizeV = 128
 sizeD = 1
 segmentName2 = 'UpperFace_cat_NEW_mod'
 sizeH2 = 32
@@ -284,7 +298,7 @@ sizeV2 = 32
 sizeD2 = 30
 testtype = "kfold"
 ###################################
-notes="modxmod"
+notes="up"
 ####################################
 
 # Load training images and labels that are stored in numpy array
