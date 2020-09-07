5d3a1ca1212cccee65eed4ddba97bfa653e9dd54
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-09-07 22:26:02 +0800

    Update SIMC_UFFMEDN_prelu_single_128_activations.py

--- Fuse/SIMC/SIMC_UFFMEDN_prelu_single_128_activations.py
+++ Fuse/SIMC/SIMC_UFFMEDN_prelu_single_128_activations.py
@@ -156,15 +156,43 @@ def evaluate(segment_train_images, segment_validation_images, segment_train_labe
     cfm = confusion_matrix(validation_labels, predictions_labels)
     print (cfm)
     print("accuracy: ",accuracy_score(validation_labels, predictions_labels))
-    activation_model = models.Model(inputs=model.input,
-                                    outputs=model.layers[1].output)
+    layer_outputs = [layer.output for layer in model.layers[:12]]  
+    activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
 
     activations = activation_model.predict([segment_validation_images, segment_validation_images_cat])
-    first_layer_activation = activations[0]
-    print(first_layer_activation.shape)
-    for i in range (16):
-        plt.matshow(first_layer_activation[i, :, :], cmap='viridis')
-        plt.show()
+    # first_layer_activation = activations[0]
+    # print(first_layer_activation.shape)
+    # for i in range (16):
+    #     plt.matshow(first_layer_activation[i, :, :], cmap='viridis')
+    # plt.show()
+    layer_names = []
+    for layer in model.layers[:3]:
+        layer_names.append(layer.name)  # Names of the layers, so you can have them as part of your plot
+
+    images_per_row = 16
+    for layer_name, layer_activation in zip(layer_names, activations):  # Displays the feature maps
+        n_features = layer_activation.shape[-1]  # Number of features in the feature map
+        size = layer_activation.shape[1]  # The feature map has shape (1, size, size, n_features).
+        n_cols = n_features // images_per_row  # Tiles the activation channels in this matrix
+        display_grid = numpy.zeros((size * n_cols, images_per_row * size))
+        for col in range(n_cols):  # Tiles each filter into a big horizontal grid
+            for row in range(images_per_row):
+                channel_image = layer_activation[0,
+                                :, :,
+                                col * images_per_row + row]
+                channel_image -= channel_image.mean()  # Post-processes the feature to make it visually palatable
+                channel_image /= channel_image.std()
+                channel_image *= 64
+                channel_image += 128
+                channel_image = numpy.clip(channel_image, 0, 255).astype('uint8')
+                display_grid[col * size: (col + 1) * size,  # Displays the grid
+                row * size: (row + 1) * size] = channel_image
+        scale = 1. / size
+        plt.figure(figsize=(scale * display_grid.shape[1],
+                            scale * display_grid.shape[0]))
+        plt.title(layer_name)
+        plt.grid(False)
+        plt.imshow(display_grid, aspect='auto', cmap='viridis')
     return accuracy_score(validation_labels, predictions_labels), validation_labels, predictions_labels
 
 
