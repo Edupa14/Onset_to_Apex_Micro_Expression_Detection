b4e43ca4d2f23dc849a8fca1fe8234d51dab4559
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-09-10 07:39:11 +0800

    Update SIMC_UFFMEDN_prelu_single_128.py

--- Fuse/SIMC/SIMC_UFFMEDN_prelu_single_128.py
+++ Fuse/SIMC/SIMC_UFFMEDN_prelu_single_128.py
@@ -138,7 +138,7 @@ def evaluate(segment_train_images, segment_validation_images, segment_train_labe
 
     # Training the model
 
-    history = model.fit([segment_train_images,segment_train_images_cat], segment_train_labels, validation_data = ([segment_validation_images,segment_validation_images_cat], segment_validation_labels), callbacks=callbacks_list, batch_size = 8, nb_epoch = 500, shuffle=True,verbose=1)
+    history = model.fit([segment_train_images,segment_train_images_cat], segment_train_labels, validation_data = ([segment_validation_images,segment_validation_images_cat], segment_validation_labels), callbacks=callbacks_list, batch_size = 12, nb_epoch = 500, shuffle=True,verbose=1)
 
 
 
@@ -301,7 +301,7 @@ sizeD2 = 30
 testtype = "kfold"
 ###################################
 
-notes="no input both (20, 20), strides=(10, 10) dropout .5 WITH 5 8"
+notes="no input both (20, 20), strides=(10, 10) dropout .5 WITH 5 12"
 
 
 # Load training images and labels that are stored in numpy array
