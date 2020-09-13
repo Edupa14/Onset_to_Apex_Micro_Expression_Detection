76a373e351967fadfb11f9fb70c18b7c1c30277a
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-09-13 14:03:39 +0800

    Update SIMC_UFFMEDN_prelu_single_128.py

--- Fuse/SIMC/SIMC_UFFMEDN_prelu_single_128.py
+++ Fuse/SIMC/SIMC_UFFMEDN_prelu_single_128.py
@@ -302,7 +302,7 @@ segmentName2 = 'UpperFace_cat_NEW_mod'
 sizeH2 = 32
 sizeV2 = 32
 sizeD2 = 30
-testtype = "kfold"
+testtype = "loocv"
 ###################################
 
 notes="no input both (20, 20), strides=(10, 10) dropout .5 WITH 5 16 long"
